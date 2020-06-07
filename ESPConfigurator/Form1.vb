a9c38d304c0c9f2cddb09d0d51adf463c452c9a7
Alessandro Mandelli <mandelli.alessandro@ngi.it>
2020-06-07 08:20:06 +0200

    Commit rilasio

--- ESPConfigurator/Form1.vb
+++ ESPConfigurator/Form1.vb
@@ -7,6 +7,37 @@ Public Class Form1
 
             Dim sock As TcpClient = New TcpClient()
             Dim address As Net.IPAddress = Net.IPAddress.Parse(txtIP.Text)
+
+            Dim test As Net.IPAddress = New Net.IPAddress(0)
+
+            Net.IPAddress.TryParse(txtlocalIP.Text, test)
+            txtlocalIP.Text = test.ToString
+            Net.IPAddress.TryParse(txtlocalGW.Text, test)
+            txtlocalGW.Text = test.ToString
+            Net.IPAddress.TryParse(txtlocalSN.Text, test)
+            txtlocalSN.Text = test.ToString
+            Net.IPAddress.TryParse(txtlocalDN1.Text, test)
+            txtlocalDN1.Text = test.ToString
+            Net.IPAddress.TryParse(txtlocalDN2.Text, test)
+            txtlocalDN2.Text = test.ToString
+
+
+            If address.ToString = "" Then
+                Throw New Exception("Indirizzo IP dell'access point non valido")
+            ElseIf Not Net.IPAddress.TryParse(txtlocalIP.Text, test) Then
+                Throw New Exception("Indirizzo IP locale non valido")
+            ElseIf Not Net.IPAddress.TryParse(txtlocalGW.Text, test) Then
+                Throw New Exception("Indirizzo IP del gateway  non valido")
+            ElseIf Not Net.IPAddress.TryParse(txtlocalSN.Text, test) Then
+                Throw New Exception("Subnect mask non valida")
+            ElseIf Not Net.IPAddress.TryParse(txtlocalDN1.Text, test) Then
+                Throw New Exception("Indirizzo IP del DNS primario non valido")
+            ElseIf Not Net.IPAddress.TryParse(txtlocalDN2.Text, test) Then
+                Throw New Exception("Indirizzo IP del DNS secondario non valido")
+            ElseIf txtSSID.Text = "" Then
+                Throw New Exception("Nome del SSID non valido.")
+            End If
+
             sock.Connect(address, 80)
 
             If sock.Connected Then
@@ -15,31 +46,27 @@ Public Class Form1
                 stream = sock.GetStream
                 If stream.CanWrite Then
 
-
-                    If txtSSID.Text <> "" Then
-                        'astream.Write(Array As Byte(MAGIC_EEPROM))
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(txtSSID.Text), 0, txtSSID.Text.Length)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
-                        Thread.Sleep(150)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(txtPassword.Text), 0, txtPassword.Text.Length)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
-                        Thread.Sleep(150)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalIP.Text), 0, txtlocalIP.Text.Length)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
-                        Thread.Sleep(150)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalGW.Text), 0, txtlocalGW.Text.Length)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
-                        Thread.Sleep(150)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalSN.Text), 0, txtlocalSN.Text.Length)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
-                        Thread.Sleep(150)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalDN1.Text), 0, txtlocalDN1.Text.Length)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
-                        Thread.Sleep(150)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalDN2.Text), 0, txtlocalDN2.Text.Length)
-                        stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
-                        Thread.Sleep(150)
-                    End If
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(txtSSID.Text), 0, txtSSID.Text.Length)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
+                    Thread.Sleep(150)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(txtPassword.Text), 0, txtPassword.Text.Length)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
+                    Thread.Sleep(150)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalIP.Text), 0, txtlocalIP.Text.Length)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
+                    Thread.Sleep(150)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalGW.Text), 0, txtlocalGW.Text.Length)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
+                    Thread.Sleep(150)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalSN.Text), 0, txtlocalSN.Text.Length)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
+                    Thread.Sleep(150)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalDN1.Text), 0, txtlocalDN1.Text.Length)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
+                    Thread.Sleep(150)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(txtlocalDN2.Text), 0, txtlocalDN2.Text.Length)
+                    stream.Write(System.Text.Encoding.UTF8.GetBytes(vbLf), 0, 1)
+                    Thread.Sleep(150)
                 Else
                     ToolStripStatusLabel1.Text = "Impossibile stabilire la connessione"
                 End If
